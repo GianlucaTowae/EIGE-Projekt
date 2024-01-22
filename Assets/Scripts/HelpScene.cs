8f293a7597176e326b542070dccd6dad8262a4d3
Gianluca Towae <towae.gianluca@gmail.com>
2024-01-23 00:36:22 +0100

    more help scene pages

--- Assets/Scripts/HelpScene.cs
+++ Assets/Scripts/HelpScene.cs
@@ -1,11 +1,55 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HelpScene : MonoBehaviour
 {
-    void Update()
+    [SerializeField] private GameObject[] pages;
+    [SerializeField] private Image next;
+    [SerializeField] private Image previous;
+    private int _page;
+
+    private void Start()
+    {
+        SetPage(0);
+        next.GetComponent<Button>().onClick.AddListener(NextPage);
+        previous.GetComponent<Button>().onClick.AddListener(PreviousPage);
+    }
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             SceneManager.LoadScene("StartScene");
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            NextPage();
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            PreviousPage();
+    }
+
+    private void PreviousPage()
+    {
+        SetPage(_page - 1);
+    }
+
+    private void NextPage()
+    {
+        SetPage(_page + 1);
+    }
+
+    private void SetPage(int pageIndex)
+    {
+        if (pageIndex < 0 || pageIndex >= pages.Length)
+            return;
+
+        _page = pageIndex;
+        foreach (GameObject page in pages)
+        {
+            page.SetActive(false);
+        }
+        pages[_page].SetActive(true);
+
+        previous.enabled = _page != 0;
+        next.enabled = _page != pages.Length - 1;
     }
 }
